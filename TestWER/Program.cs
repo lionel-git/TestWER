6be530ba9caa37203af2ad127dcab3960dbf5a4b
Lionel Desorme <lionel.desorme@free.fr>
2019-06-15 13:33:40 +0200

    Initial commit

---
+++ TestWER/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestWER
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test Core File...");
+            throw new Exception("Some uncatched exception ...");
+        }
+    }
+}
